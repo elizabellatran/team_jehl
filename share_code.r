9b61de43ed333aac9ba80f92e15be34c4811fa2f
Lori Ling <lorraine.ling@gmail.com>
2019-04-13 23:32:22 -0700

    redo conversion of dataframe
    only  character strings were converted to factors and then to variables

--- share_code.r
+++ share_code.r
@@ -14,8 +14,8 @@
 library(knitr)
 library(tidyverse)
 library(reshape2)
-library(dplyr)
-library(ggplot2)
+library(ggbiplot)
+
 
 #########
 # set WD # 
@@ -26,8 +26,35 @@ setwd('/Users/elizabella/Projects/BlueprintHack/')
 # Data #
 ####### 
 
-org_df = read_csv('stanford_blueprint_datathon_2019_data.csv')
+STD = read_csv('stanford_blueprint_datathon_2019_data.csv')
 var_interest= ('BRFSS Variables of Interest.xlsx')
 
 ########
 
+# In order to run PCA, need to convert all strings into numeric
+DF_exp<-STD[, 1:68] #subset all the exposure variables
+colnames(DF_exp)
+str(DF_exp)
+for (i in colnames(DF_exp)) {
+  print(i)
+  if (is.character(DF_exp[[i]]) == TRUE) {
+    j <- as.factor(DF_exp[[i]]) %>% as.numeric()
+    #print(j)
+    DF_exp[[i]] <- j 
+  }
+}
+
+pca_exposure = prcomp(na.omit(DF_exp), center = TRUE, scale. = TRUE)
+summary(pca_exposure)
+
+pca_rotation<-as.data.frame(pca_exposure$rotation)
+write_csv(pca_rotation, "Exposures_PCA_rotations_v2.csv")
+
+# Make some plots !
+plot(pca_exposure, main = "variances of the principle components") # ranked graph of PC axes
+ggsave("Variances_of_PCA.png")
+
+ggbiplot(pca_exposure, obs.scale =1, var.scale=1,
+            ellipse =FALSE, labels = NULL, var.axes = TRUE) + 
+  theme_classic() +
+  ggtitle("PCA of exposure variables") 
